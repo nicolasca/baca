ee9bbdab6263f40443bd93d988bde326d8659b21
Nicolas Castejon <ncastejon.externe@bedrockstreaming.com>
2022-05-25 10:31:07 +0200

    First push with cards

---
+++ pages/cards.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Cards() {
+  return (
+    <>
+      <h1>Les cartes</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </>
+  );
+}
